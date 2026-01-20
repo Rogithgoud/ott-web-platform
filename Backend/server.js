6ac44dee5f602c1592535de767919eef9f5e1192
Rogithgoud <rogithgoud@gmail.com>
2026-01-20 16:17:06 +0530

    Fix CORS for Vercel with credentials

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cookieParser from "cookie-parser";
-import cors from "cors";
+
 
 import authRoutes from "./routes/auth.route.js";
 import movieRoutes from "./routes/movie.route.js";
@@ -17,12 +17,35 @@ connectDB();
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(
-  cors({
-    origin: true,
-    credentials: true,
-  })
-);
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+
+  const allowedOrigins = [
+    "http://localhost:5173",
+    "https://ott-web-platform-frontend.vercel.app",
+  ];
+
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,POST,PUT,DELETE,OPTIONS"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization"
+  );
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
+
 
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/movie", protectRoute, movieRoutes);
--- Backend/utils/generateToken.js
+++ Backend/utils/generateToken.js
@@ -3,7 +3,7 @@ import { ENV_VARS } from "../config/envVars.js";
 
 export const generateTokenAndSetCookie= (userId,res) =>{
     const token= jwt.sign({userId}, ENV_VARS.JWT_SECRET,{expiresIn:"15d"});
-    res.clearcookie("jwt-netflix",token,{
+    res.cookie("jwt-netflix",token,{
         maxAge:15*24*60*60*1000, //15 days in ms
         httpOnly:true,    //prevents XSS attacks cross site scripting attacks,make it not be accessed by js
         sameSite:"none", //CSRF attacks cross-site request forgery attacks
