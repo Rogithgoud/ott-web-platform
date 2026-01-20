8c0494a82a967044e0dfa843cba6248d26da4ce0
Rogithgoud <rogithgoud@gmail.com>
2026-01-20 16:11:04 +0530

    Fix CORS and DB connection for Vercel

--- Backend/server.js
+++ Backend/server.js
@@ -1,48 +1,32 @@
-import express from 'express';
-import cookieParser from 'cookie-parser';
+import express from "express";
+import cookieParser from "cookie-parser";
 import cors from "cors";
-
 
 import authRoutes from "./routes/auth.route.js";
 import movieRoutes from "./routes/movie.route.js";
 import tvRoutes from "./routes/tv.route.js";
 import searchRoutes from "./routes/search.route.js";
 
-import { ENV_VARS } from './config/envVars.js';
-import { connectDB } from './config/db.js';
-import { protectRoute } from './middleware/protectRoute.js';
+import { connectDB } from "./config/db.js";
+import { protectRoute } from "./middleware/protectRoute.js";
 
-const app=express(); 
-const PORT = ENV_VARS.PORT
+const app = express();
 
-app.use(express.json());//will allow us to parse req.body
-app.use(cookieParser()); 
+connectDB(); 
+
+app.use(express.json());
+app.use(cookieParser());
 
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "https://ott-web-platform-frontend.vercel.app"
-    ],
+    origin: true,
     credentials: true,
   })
 );
 
-
-app.use("/api/v1/auth",authRoutes);
-app.use("/api/v1/movie",protectRoute,movieRoutes);
-app.use("/api/v1/tv",protectRoute,tvRoutes);
-app.use("/api/v1/search",protectRoute,searchRoutes);
-
-console.log("MONGO_URI: ", process.env.MONGO_URI);
-
-if (process.env.NODE_ENV !== "production") {
-  app.listen(PORT, () => {
-    console.log("Server running on port " + PORT);
-    connectDB();
-  });
-}
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/movie", protectRoute, movieRoutes);
+app.use("/api/v1/tv", protectRoute, tvRoutes);
+app.use("/api/v1/search", protectRoute, searchRoutes);
 
 export default app;
-
-
