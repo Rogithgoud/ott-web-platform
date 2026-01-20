8878738cccebc9b9f19f29ab5846a86df1573e04
Rogithgoud <rogithgoud@gmail.com>
2026-01-20 16:21:32 +0530

    Fix CORS and cookies

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cookieParser from "cookie-parser";
-
+import cors from "cors";
 
 import authRoutes from "./routes/auth.route.js";
 import movieRoutes from "./routes/movie.route.js";
@@ -12,40 +12,17 @@ import { protectRoute } from "./middleware/protectRoute.js";
 
 const app = express();
 
-connectDB(); 
+connectDB();
 
 app.use(express.json());
 app.use(cookieParser());
 
-app.use((req, res, next) => {
-  const origin = req.headers.origin;
-
-  const allowedOrigins = [
-    "http://localhost:5173",
-    "https://ott-web-platform-frontend.vercel.app",
-  ];
-
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader("Access-Control-Allow-Origin", origin);
-  }
-
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET,POST,PUT,DELETE,OPTIONS"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization"
-  );
-
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(200);
-  }
-
-  next();
-});
-
+app.use(
+  cors({
+    origin: "https://ott-web-platform-frontend.vercel.app",
+    credentials: true,
+  })
+);
 
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/movie", protectRoute, movieRoutes);
